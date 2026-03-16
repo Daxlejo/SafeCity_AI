88c99e6d0f6fb6ad60d54c6f30a95ef606fc4d03
Edgar <edgar.arteagareal@campusucc.edu.co>
2026-03-16 16:55:51 -0500

    feat: crear entidad Report, Enums y ReportRepository

---
+++ backend/src/main/java/com/safecityai/backend/model/enums/IncidentType.java
@@ -0,0 +1,13 @@
+package com.safecityai.backend.model.enums;
+
+public enum IncidentType {
+    ROBBERY,
+    ASSAULT,
+    THEFT,
+    ACCIDENT,
+    TRAFFIC,
+    TRANSIT_OP,
+    SUSPICIOUS,
+    VANDALISM,
+    OTHER
+}
